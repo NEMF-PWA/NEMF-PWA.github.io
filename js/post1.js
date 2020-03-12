6dec169a93a9ca63546edca5b887ac435be37636
vijaymshr5 <vijaymshr5@gmail.com>
2020-03-12 16:27:29 +0530

    Update post1.js

--- js/post1.js
+++ js/post1.js
@@ -44,6 +44,14 @@ function initFirebaseMessagingRegistration() {
         });
 
     messaging.onMessage(function(payload) {
+		const notificationTitle = payload.notification.title;
+		const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,        
+		};
+		navigator.serviceWorker.ready.then(function(registration){
+			registration.showNotification(title, options);
+		});
         console.log("Message received. ", JSON.stringify(payload));
     });
     messaging.onTokenRefresh(function() {
