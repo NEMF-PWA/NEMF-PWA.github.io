527a0f835b02301c05f7d6b4de893412edd39379
vijaymshr5 <vijaymshr5@gmail.com>
2020-03-23 17:51:22 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,60 @@ window.onload = () => {
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('./sw.js');
-    }
+            .register('./sw.js').then(function(){
 
+    firebase.initializeApp({
+        apiKey: "AIzaSyBtB3nk9k-AUuBQvcplmUbdwI13HIWZT6U",
+        authDomain: "notification-test-25d1e.firebaseapp.com",
+        databaseURL: "https://notification-test-25d1e.firebaseio.com",
+        projectId: "notification-test-25d1e",
+        storageBucket: "notification-test-25d1e.appspot.com",
+        messagingSenderId: "827645719830",
+        appId: "1:827645719830:web:955b36b22b04bebdb60c37"
+    });
+    const messaging = firebase.messaging();
+    messaging.usePublicVapidKey("BEbXWzdWqkgklIezwpMt5q4l_ru1mopNuDQKVNm1kp9pce8dhCiWNxZWUNV7VnL2mHudTnr4he_ipzE3vKteWW0");
+    messaging
+        .requestPermission()
+        .then(function() {
+            console.log("Got notification permission");
+            return messaging.getToken();
+        })
+        .then(function(token) {
+            // print the token on the HTML page
+            console.log("Token is ", token);
+			$('#notificationToken').text(token);
+        })
+        .catch(function(err) {
+            console.log("Didn't get notification permission", err);
+        });
+
+    messaging.onMessage(function(payload) {
+		/*const notificationTitle = payload.notification.title;
+		const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,        
+		};*/
+		/*setTimeout(() => {
+		navigator.serviceWorker.ready.then(function(registration){
+			registration.active.postMessage(payload);
+		}, 100);
+		});*/
+		
+		navigator.serviceWorker.controller.postMessage(payload);
+        console.log("Message received. ", JSON.stringify(payload));
+        console.log("Message received. ", navigator.serviceWorker.controller);
+    });
+    messaging.onTokenRefresh(function() {
+        messaging.getToken()
+            .then(function(refreshedToken) {
+                console.log('Token refreshed.');
+            }).catch(function(err) {
+                console.log('Unable to retrieve refreshed token ', err);
+            });
+    });				
+			});
+    }
 
     $("#loginBtn").click($.proxy(function (event) {
         if ($('#username').val() == '') {
@@ -27,5 +78,10 @@ window.onload = () => {
         }
     }));
 
+
+}
+
+
+function initFirebaseMessagingRegistration() {
 
 }
