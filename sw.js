a699f60bc45231cf32133cc3d50080f2fb2a1759
vijaymshr5 <vijaymshr5@gmail.com>
2020-03-24 16:59:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -62,9 +62,9 @@ self.addEventListener('notificationclick', function (e) {
     // Get all the Window clients
     e.waitUntil(clients.matchAll({type: 'window'}).then(clientsArr => {
         // If a Window tab matching the targeted URL already exists, focus that;
-        const hadWindowToFocus = clientsArr.some(windowClient => windowClient.url === e.notification.data.url ? (windowClient.focus(), true) : false);
+        const hadWindowToFocus = clientsArr.some(windowClient => windowClient.url === e.notification.data.click_action ? (windowClient.focus(), true) : false);
         // Otherwise, open a new tab to the applicable URL and focus it.
-        if (!hadWindowToFocus) clients.openWindow(e.notification.data.url).then(windowClient => windowClient ? windowClient.focus() : null);
+        if (!hadWindowToFocus) clients.openWindow(e.notification.data.click_action).then(windowClient => windowClient ? windowClient.focus() : null);
     }));
 });
 
